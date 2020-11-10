1b20006572d6fdad10ed5341024e8b7f4ade1593
Ahron Kwon <kwonahron@gmail.com>
2020-11-10 12:09:19 +0900

    Add delete Method in NodeMangerClass

--- tree.js
+++ tree.js
@@ -23,23 +23,26 @@ class Node{
     this.value = value;
     this.left = null;
     this.right = null;
-    this.depth = null;
+    this.depth = 1;
   }
 }
 
 class NodeManager{
   constructor(root){
     this.root = root;
+    this.allData = [this.root.value];
+    this.allDataCount = 1;
   }
 
   insert(value){
+    this.allData[this.allDataCount++] = value
     let currentNode = this.root;
-    let tempDepth = 1;
+    let tempDepth = 2;
     while(true){
       if(value < currentNode.value){
         if(currentNode.left !== null){
           currentNode = currentNode.left;
-          tempDepth++
+          ++tempDepth
         }else{
           currentNode.left = new Node(value);
           currentNode.left.depth = tempDepth;
@@ -48,7 +51,7 @@ class NodeManager{
       }else{
         if(currentNode.right !== null){
           currentNode = currentNode.right;
-          tempDepth++
+          ++tempDepth
         }else{
           currentNode.right = new Node(value);
           currentNode.right.depth = tempDepth;
@@ -56,6 +59,99 @@ class NodeManager{
         }
       }
     }
+  }
+
+  // 와씨 만드는건 쉬운데 어떻게 없에지...
+  //scenario
+  // 1. Leaf node 일때... (자식이 없다.)
+// 2. Child node 1개일때... (자식이 하나)
+// 3. child node 2개일때... (자식이 둘)
+
+  delete(value){
+    let searched = false;
+    let currentNode = this.root;
+    let parentNode = this.root;
+    while(currentNode){
+      if(currentNode.value == value){
+        searched = true;
+        break;
+      }else if(value < currentNode.value){
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      }else{
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      }
+    }
+    if(searched == false){
+      return false;
+    }
+    //case1 node가 leaf node 일때
+    if(currentNode.left == null && currentNode.right == null){
+      if(value < parentNode){
+        parentNode.left = null;
+      }else{
+        parentNode.right = null;
+      }
+      return searched;
+    }
+    //case2 child node가 한개 일때
+    //1. 왼쪽에 가지고 있을 때
+    //2. 오른쪽에 가지고 있을 때
+    if(currentNode.left != null && currentNode.right == null){
+      if(value < parentNode.value){
+        parentNode.left = currentNode.left;
+      }else{
+        parentNode.right = currentNode.left;
+      }
+    }else if(currentNode.left == null && currentNode.right != null){
+      if(value < parentNode.value){
+        parentNode.left = currentNode.right;
+      }else{
+        parentNode.right = currentNode.right;
+      }
+      return searched;
+    }
+
+    //case3 chid node가 2개
+    if(currentNode.left != null && currentNode.right !=null){
+      //case3-1 부모 노드의 왼쪽에 있을 때
+      if(value < parentNode.value){
+        let changeNode = currentNode.right;
+        let changeNodeParent = currentNode.right;
+        while(changeNode.left != null){
+          changeNodeParent = changeNode;
+          changeNode = changeNode.left;
+        }
+        if(changeNode.right != null){
+          changeNodeParent.left = changeNode.right;
+        }else{
+          changeNodeParent.left = null;
+        }
+        parentNode.left = changeNode;
+        changeNode.right = currentNode.right;
+        changeNode.left = currentNode.left;
+        return searched;
+      }//case 3-2 부모 노드의 오른쪽에 있을 때
+      else{
+        let changeNode = currentNode.right;
+        let changeNodeParent =currentNode.right;
+        while(changeNode.left != null){
+          changeNodeParent = changeNode;
+          changeNode = changeNode.left;
+        }
+        if(changeNode.right != null){
+          changeNodeParent.left = changeNode.right;
+        }else{
+          changeNodeParent.left = null;
+        }
+        parentNode.right = changeNode;
+        changeNode.left = currentNode.left;
+        changeNode.right = currentNode.right;
+        return searched;
+      }
+    }
+
   }
 
   search(value){
@@ -69,17 +165,22 @@ class NodeManager{
         currentNode = currentNode.right
       }
     }
-    return false 
+    return currentNode;
   }
 }
 
-const rootNode = new Node(3);
-const BST = new NodeManager(rootNode);
-BST.insert(2);
-BST.insert(4);
-BST.insert(5);
-BST.insert(6);
+//test code
+const randomValue = [];
+let count = 0;
+while(randomValue.length <= 100){
+  randomValue[count] = Math.floor(Math.random()*1000);
+  count++;
+}
+const node = new Node(500);
+const BST = new NodeManager(node);
 
+for(let i of randomValue){
+  BST.insert(i);
+}
 
-console.log(BST.root);
-console.log(BST.search(6))
+console.log(BST.allData)
